70960aadf3c699b52f4b043048d0717e904d054f
Koding <conmute@conmute.koding.io>
2016-02-20 20:09:07 -0600

    layout start

--- victims/__init__.py
+++ victims/__init__.py
@@ -6,5 +6,7 @@ app = Flask(__name__,
             instance_path=get_config_folder_path(),
             instance_relative_config=True,
             template_folder='templates')
-            
+
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
 app.register_blueprint(main, url_prefix='/')
--- victims/main/controllers.py
+++ victims/main/controllers.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, current_app, render_template
 import time
 
-main = Blueprint('main', __name__, template_folder='templates')
+main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     content = current_app.config.get('MODE') + ' | '+ time.ctime()
-    return render_template('home.htm', content=content)
+    return render_template('home.jade', content=content)
